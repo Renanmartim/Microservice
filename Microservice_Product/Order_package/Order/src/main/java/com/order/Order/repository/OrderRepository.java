d76b05f63d99f8fb0494fc31ce13d636380f79a4
Renan Martim <117313515+Renanmartim@users.noreply.github.com>
2023-09-09 20:45:11 -0300

    Delete Microservice_Product directory

--- Microservice_Product/Order_package/Order/src/main/java/com/order/Order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.order.Order.repository;
-
-import com.order.Order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
