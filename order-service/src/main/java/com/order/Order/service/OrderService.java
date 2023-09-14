993b2e95503b93b58da634bd225f63d1f67caf60
Renan Martim <117313515+Renanmartim@users.noreply.github.com>
2023-09-14 04:28:19 -0300

    Add Loar Balancer and modify end point.

--- order-service/src/main/java/com/order/Order/service/OrderService.java
+++ order-service/src/main/java/com/order/Order/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public ResponseEntity<Order> saveOrder(OrderRequest orderRequest) throws Exception {
         Order order = new Order();
@@ -42,8 +42,8 @@ public class OrderService {
         System.out.println("SKU Codes: " + getSkuCodes);
 
 
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", getSkuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
