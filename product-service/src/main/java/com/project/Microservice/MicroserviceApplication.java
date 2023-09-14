8d8c6edeb653d727c314aed1f6f83838365e2b9f
Renan Martim <117313515+Renanmartim@users.noreply.github.com>
2023-09-14 02:08:20 -0300

    Add Eureka server and packages clients and add new features.

--- product-service/src/main/java/com/project/Microservice/MicroserviceApplication.java
+++ product-service/src/main/java/com/project/Microservice/MicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.project.Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
